e82bcb69cea0a2be9be856613d77cbc5bf8ea1cf
Maxim_D <dahovnik@gmail.com>
2021-04-22 14:31:34 +0200

    Добавьте файлы проекта.

---
+++ PayrollPreparationConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollPreparationConsole
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
